047fc4be9b2ff3baae683e56e0267ed273b8dca2
guilhermesdias45 <guilherme.sdias@sptech.school>
2025-03-23 15:45:11 -0300

    Separação de funções

---
+++ funcional.js
@@ -0,0 +1,15 @@
+function Mostrar(){
+        
+}
+
+function Converter(){
+    base = Number(ipt_tipo_base.value);
+    numero = Number(ipt_numero.value);
+    mensagem_base2 = `<br>base 2: ${numero.toString(2)}`;
+    mensagem_base8 = `<br>base 8: ${numero.toString(8)}`;
+    mensagem_base10 = `<br>base 10: ${numero.toString(10)}`;
+    mensagem_base16 = `<br>base 16: ${numero.toString(16)}`;
+    resp = caixa_resposta;
+
+    
+}
