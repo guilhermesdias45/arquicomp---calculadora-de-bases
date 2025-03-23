c0725f1c08e13d8f406b080e6a047447a5b8e4e3
guilhermesdias45 <guilherme.sdias@sptech.school>
2025-03-23 16:16:57 -0300

    o mundo não nos ama

--- funcional.js
+++ funcional.js
@@ -8,7 +8,7 @@ function Converter(){
     mensagem_base2 = `<br>base 2: ${numero.toString(2)}`;
     mensagem_base8 = `<br>base 8: ${numero.toString(8)}`;
     mensagem_base10 = `<br>base 10: ${numero.toString(10)}`;
-    mensagem_base16 = `<br>base 16: ${numero.toString(16)}<br> OLA MUNDO`;
+    mensagem_base16 = `<br>base 16: ${numero.toString(16)}<br> OLA MUNDO, o mundo não nos ama`;
     resp = caixa_resposta;
 
     
